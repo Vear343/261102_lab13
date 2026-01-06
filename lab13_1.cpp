6bfacf273131988e652021b01ba67e51474ff939
Vear <arnuwat.vear@gmail.com>
2026-01-06 16:17:27 +0700

    lab13_1 finished

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,31 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N, double B[]){
+    double am,sd,gm,hm;
+    double max = A[0];
+    double min = A[0];
+    double sum = 0;
+    double sum_thing = 0;
+    double sumx = 1;
+    double har_sum = 0;
+    for (int i = 0; i < N; i++){
+        sum += A[i];
+        sum_thing += pow(A[i],2);
+        sumx *= A[i];
+        har_sum += 1.00/A[i];
+        if(A[i] > max) max = A[i];
+        if(A[i] < min) min = A[i];
+    }
+    am = sum/N;
+    sd = sqrt(sum_thing/N - pow(am,2));
+    gm = pow(sumx,1.00/N);
+    hm = N/har_sum;
+    B[0] = am;
+    B[1] = sd;
+    B[2] = gm;
+    B[3] = hm;
+    B[4] = max;
+    B[5] = min;
+}
